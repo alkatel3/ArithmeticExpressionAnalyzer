ea1275677e8985c533ff6088bbdf0a8693805485
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2024-09-25 22:38:10 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿namespace ArithmeticExpressionAnalyzer
+{
+    public enum TokenType
+    {
+        function,
+        operation, 
+        digit,
+        variable,
+        openBrake,
+        closeBrake,
+        unknown
+    }
+
+    internal class Program
+    {
+        static Tokenizer tokenizer = new Tokenizer();
+        static Dictionary<TokenType, Func<string, string, (bool, string)>> TokensValidation = new Dictionary<TokenType, Func<string, string, (bool, string)>>
+        {
+            [TokenType.digit] = IsOperandPositionValid,
+            [TokenType.operation] = IsOperationPositionValid,
+            [TokenType.function] = IsOperandPositionValid,
+            [TokenType.variable] = IsOperandPositionValid,
+            [TokenType.openBrake] = IsOpenBrakePositionValid,
+            [TokenType.closeBrake] = IsCloseBrakePositionValid,
+            [TokenType.unknown] = (item1, item2) => (false, $"Не типовий символ '{item1}'")
+        };
+        static List<(int, string)> errors = new List<(int, string)>();
+        static List<(int, string)> errorsMessages= new List<(int, string)>();
+        static Stack<int> brakes = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.WriteLine("Введіть вираз:");
+                var exp = Console.ReadLine();
+                exp = exp.Replace(" ", "");
+                var res = tokenizer.Tokenize(exp);
+                string? previous = null;
+                for (int i = 0; i < res.Count; i++)
+                {
+                    var token = res[i];
+                    var tokenType = tokenizer.CheckTokenType(token.value);
+                    (bool res, string error) isValid;
+
+                    if (tokenType == TokenType.openBrake)
+                        brakes.Push(token.index);
+
+                    isValid = TokensValidation[tokenType].Invoke(token.value, previous);
+
+                    if (!isValid.res)
+                    {
+                        errors.Add(token);
+                        errorsMessages.Add((token.index, isValid.error));
+                    }
+
+                    previous = token.value;
+                }
+
+                if (brakes.Any())
+                    foreach (var item in brakes)
+                    {
+                        errors.Add((item, "("));
+                        errorsMessages.Add((item, "Лишня відкриваюча дужка"));
+                    }
+
+                errors = errors.Distinct().OrderBy(e => e.Item1).ToList();
+                var lastIndex = 0;
+                Console.WriteLine("---------------------------------------");
+                foreach (var error in errors)
+                {
+                    Console.Write(exp.Substring(lastIndex, error.Item1 - lastIndex));
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(exp.Substring(error.Item1, error.Item2.Length));
+                    Console.ResetColor();
+                    lastIndex = error.Item1 + error.Item2.Length;
+                }
+
+                Console.WriteLine(exp.Substring(lastIndex));
+
+                errorsMessages = errorsMessages.OrderBy(e => e.Item1).ToList();
+                foreach (var errorsMessage in errorsMessages)
+                {
+                    Console.WriteLine($"Error: \"{errorsMessage.Item2}\"; Index: {errorsMessage.Item1}");
+                }
+
+                if (!errorsMessages.Any())
+                    Console.WriteLine($"Вираз правильний");
+                Console.WriteLine("---------------------------------------");
+                errors.Clear();
+                errorsMessages.Clear();
+                brakes.Clear();
+                Console.ReadKey();
+
+            }
+        }
+
+        public static (bool res, string error) IsOperandPositionValid(string token, string? previousToken)
+        {
+            if (String.IsNullOrEmpty(previousToken))
+                return (true, "");
+
+            var previousTokenType = tokenizer.CheckTokenType(previousToken);
+
+            return (previousTokenType == TokenType.operation ||
+                previousTokenType == TokenType.openBrake, $"'{token}' не може стояти після '{previousToken}'");
+        }
+
+        public static (bool res, string error) IsOperationPositionValid(string token, string? previousToken)
+        {
+            if (String.IsNullOrEmpty(previousToken) && (token == "+" || token == "-"))
+                return (true, "");
+            else if(String.IsNullOrEmpty(previousToken))
+                return (false, $"Вираз не може починатись з '{token}'");
+
+            var previousTokenType = tokenizer.CheckTokenType(previousToken);
+
+            return (previousTokenType == TokenType.digit ||
+                previousTokenType == TokenType.variable ||
+                previousTokenType == TokenType.closeBrake, $"'{token}' не може стояти після '{previousToken}'");
+        }
+
+        public static (bool res, string error) IsOpenBrakePositionValid(string token, string? previousToken)
+        {
+            if (token != "(")
+                throw new ArgumentException();
+
+            if (String.IsNullOrEmpty(previousToken))
+                return (true, "");
+
+            var previousTokenType = tokenizer.CheckTokenType(previousToken);
+
+            return (previousTokenType == TokenType.operation ||
+                previousTokenType == TokenType.function ||
+                previousTokenType == TokenType.openBrake, $"'{token}' не може стояти після '{previousToken}'");
+        }
+
+        public static (bool res, string error) IsCloseBrakePositionValid(string token, string? previousToken)
+        {
+            if (token != ")")
+                throw new ArgumentException();
+
+            if (String.IsNullOrEmpty(previousToken))
+                return (false, $"Вираз не може починатись з '{token}'");
+
+            if (!brakes.Any())
+            {
+                return(false, "Лишня закриваюча дужка");
+            }
+            else
+            {
+                brakes.Pop();
+            }
+
+            var previousTokenType = tokenizer.CheckTokenType(previousToken);
+
+            
+            return (previousTokenType == TokenType.digit ||
+                previousTokenType == TokenType.variable ||
+                previousTokenType == TokenType.closeBrake, $"'{token}' не може стояти після '{previousToken}'");
+
+        }
+    }
+}
---
+++ Tokenizer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ArithmeticExpressionAnalyzer
+{
+    public class Tokenizer
+    {
+        public string[] Funstions { get; set; }
+        public string FloatDigins { get; set; }
+        public string Digins { get; set; }
+        public string Variables { get; set; }
+        public string[] Operations { get; set; }
+        public string OpenBrake { get; set; }
+        public string CloseBrake { get; set; }
+
+        private readonly string pattern;
+
+        public Tokenizer()
+        {
+            Funstions = [ "sin", "cos", "tg" ];
+            Operations = [ "+", "-", "*", "/", "^" ];
+            FloatDigins = @"\d+\.\d+";
+            Digins = @"\d+";
+            Variables = @"[a-zA-Z]";
+            OpenBrake = @"\(";
+            CloseBrake = @"\)";
+            pattern = @$"({string.Join('|', Funstions)}|{FloatDigins}|{Digins}|{Variables}|{string.Join('|', Operations.Select(o => @"\" + o))}|{OpenBrake}|{CloseBrake})";
+        }
+
+        public List<(int index, string value)> Tokenize(string input)
+        {
+            List<(int, string)> tokens = new List<(int, string)>();
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int lastIndex = 0;
+            foreach (Match match in matches)
+            {
+                if (lastIndex < match.Index)
+                {
+                    string invalidToken = input.Substring(lastIndex, match.Index - lastIndex);
+                    tokens.Add((lastIndex, invalidToken.Trim()));
+                }
+                lastIndex = match.Index + match.Length;
+                tokens.Add((match.Index, match.Value));
+            }
+
+            if (lastIndex < input.Length)
+            {
+                string invalidToken = input.Substring(lastIndex).Trim();
+                if (!string.IsNullOrEmpty(invalidToken))
+                {
+                    tokens.Add((lastIndex, invalidToken));
+                }
+            }
+
+            return tokens;
+        }
+
+        public bool IsOperator(string token)
+        {
+            return Regex.IsMatch(token, $@"{string.Join('|', Operations.Select(o => @"\" + o))}");
+        }
+
+        public bool IsNumber(string token)
+        {
+            return Regex.IsMatch(token, $@"{FloatDigins}|{Digins}");
+        }
+
+        public bool IsVariable(string token)
+        {
+            return Regex.IsMatch(token, $@"{Variables}");
+        }
+
+        private bool IsFunction(string token)
+        {
+            return Regex.IsMatch(token, $@"\b(?:{string.Join('|', Funstions)})\b");
+        }
+
+        public TokenType CheckTokenType(string token)
+        {
+            return IsNumber(token) ? TokenType.digit :
+                IsFunction(token) ? TokenType.function :
+                IsOperator(token) ? TokenType.operation :
+                IsVariable(token) ? TokenType.variable :
+                token == "(" ? TokenType.openBrake :
+                token == ")" ? TokenType.closeBrake : TokenType.unknown;
+        }
+    }
+}
